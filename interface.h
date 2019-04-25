036916f84b089bddd82879a92c304c976466b642
Edward Yan <47233190+Edwardyan21@users.noreply.github.com>
2019-04-25 19:03:38 +0800

    Create interface.h

---
+++ interface.h
@@ -0,0 +1,16 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+/*
+To do: Welcome page+content
+content: 1. Add a financial status --> income / expense
+         2. View my status --> recent 10 details / get your report of the month-->top three, categories, 
+         3. Budgets --> Set my budgets / View my budgets / *Special events
+         4. Personal information --> Change my name / Change my password
+         5. Quit
+
+*/
+
+
+#endif
