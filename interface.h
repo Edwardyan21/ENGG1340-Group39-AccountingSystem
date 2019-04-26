abc6a69d8bf9aa5be048b06a7187b6c195b8b2fe
Edward Yan <47233190+Edwardyan21@users.noreply.github.com>
2019-04-26 23:27:05 +0800

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,16 +1,24 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<fstream>
+#include<cctype>
+#include<iomanip>
+#include ""
+#include ""
 /*
 To do: Welcome page+content
 content: 1. Add a financial status --> income / expense
-         2. View my status --> recent 10 details / get your report of the month-->top three, categories, 
+         2. View my status --> recent 10 details / get your report of the month-->top three, categories,
          3. Budgets --> Set my budgets / View my budgets / *Special events
          4. Personal information --> Change my name / Change my password
          5. Quit
-
 */
-
+void interface();
 
 #endif
