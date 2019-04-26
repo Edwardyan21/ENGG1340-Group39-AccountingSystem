f95845efcdfd139c3339a755d10e224bafa0ff90
Edward Yan <47233190+Edwardyan21@users.noreply.github.com>
2019-04-27 00:39:12 +0800

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
 
+using namespace std;
+
+void add_status()
+  
+void del_status()
+  
+void view_status()
+  
+void budgets()
