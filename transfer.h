c39f1cff3083ca30ad0982522a9d90dfd6e17c00
Edward Yan <47233190+Edwardyan21@users.noreply.github.com>
2019-04-27 11:41:55 +0800

    Update transfer.h

--- transfer.h
+++ transfer.h
@@ -1,1 +1,20 @@
+#ifndef TRANSFER_H
+#define TRANSFER_H
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
 
+string transfer_category(int);
+string transfer_account(int);
+double Calculate_money(string);
+
+
+
+
+
+#endif
