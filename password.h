1898cfc85296aa9947d166a071b36c33312f8e93
Jenny Chen <43599817+Jieying-Chen@users.noreply.github.com>
2019-04-27 09:53:40 +0800

    Update password.h

--- password.h
+++ password.h
@@ -1,1 +1,20 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
 
+bool is_firsttime();
+
+void store_username(string);
+
+string enc(string);
+
+void store_userkeys(string);
+
+string get_username();
+
+string get_userkeys();
+
+#endif
