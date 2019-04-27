b24120ed0f1028812b4e93f44626e47f184f095d
Edward Yan <47233190+Edwardyan21@users.noreply.github.com>
2019-04-27 11:43:24 +0800

    Update customer.h

--- customer.h
+++ customer.h
@@ -8,6 +8,7 @@
 #include<iomanip>
 #include<fstream>
 #include<cctype>
+#include "transfer.h"
 
 class record
 {
@@ -17,17 +18,19 @@ class record
     int get_date() { return date; }
     int get_cat() { return category; }
     double get_amo() { return amount; }
+    int get_acc() { return account; }
+    
     void set_cur(string s) { currency = s;}
     void set_month(int num) { month = num; }
     void set_date(int num) { date = num; }
     void set_cat(int num) { category = num; }
     void set_amo(double num) { amount = num; }
-    
-    
-    
+    void set_acc(int num) { account = num; }
+
+
   private:
     string currency;
-    int month, date, category;
+    int month, date, category, account;
     double amount;
 };
 
